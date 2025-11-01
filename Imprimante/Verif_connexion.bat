8f74bbf6dc4f56fc36d9dc20a9205b4dc5e7a05b
youssef <youssef.el-kattoufi@universite-paris-saclay.fr>
2025-11-01 16:25:45 +0100

    Amélioration de verif connexion

--- Imprimante/Verif_connexion.bat
+++ Imprimante/Verif_connexion.bat
@@ -1,12 +1,24 @@
 @echo off
-ping -4 EPSON3581C8.local -n 1 >nul 2>&1
+set "WAIT_SEC=10"
+echo Test de la connexion internet avec l'imprimante
+
+REM timeout /t %WAIT_SEC% >nul 2>&1
+
+REM ping -4 EPSON3581C8.local -n 4 >nul 2>&1
+
+REM Permet de pign l'imprimante
+ping -4 EPSON3581C8.local -n 4
+
+REM Permet de sauter de 2 lignes
+echo.
+echo.
 
 if errorlevel 1 (
     echo La connexion avec l'imprimante n'est pas detectee.
 ) else (
     echo La connexion avec l'imprimante est detectee.
 )
-
-echo Taper entrée pour fermer la page.
+echo.
+echo Taper sur une touche pour fermer la page.
 pause >nul
 
